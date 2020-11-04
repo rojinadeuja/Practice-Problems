9987281efad086d8aec3c9c239463952a2d24f98
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-03 22:50:20 -0600

    Add comments

--- 20-Valid-Parenthesis.py
+++ 20-Valid-Parenthesis.py
@@ -1,3 +1,11 @@
+'''
+Leetcode Q20 -https://leetcode.com/problems/valid-parentheses/
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+'''
 class Solution:
     def isValid(self, s: str) -> bool:
         dct = {'(':')', '{':'}', '[':']'}
@@ -6,12 +14,15 @@ class Solution:
             return False
         for bracket in s:
             if bracket in dct:
+                # If opening tag then push into stack
                 stack.append(bracket)
             else:
+                # If stack is not empty and last element is the closing tag for a bracket then pop out of stack
                 if(stack and dct[stack[-1]] == bracket):
                     stack.pop()
                 else:
                     return False
+        # Return true only if stack is empty
         if not stack:
             return True
                     
