05f08b0e8f5484e3aca548d23129e13fb5011be0
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-03 21:52:04 -0600

    Update Q28

--- 28-Implement-strStr().py
+++ 28-Implement-strStr().py
@@ -1,3 +1,8 @@
+'''
+Leetcode Q28 -https://leetcode.com/problems/implement-strstr/
+Implement strStr().
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+'''
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if needle == '':
