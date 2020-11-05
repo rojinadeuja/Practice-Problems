2e3f0ad5b4de87292078df6648c38418a783ad42
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-05 09:35:50 -0600

    Add Q257

---
+++ 257-Binary-Path-Trees.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+        result = []
+        
+        def dfs(root, path):
+            if not root.left and not root.right:
+                result.append(path)
+                return
+            if root.left:
+                dfs(root.left, path + '->' + str(root.left.val))
+            if root.right:
+                dfs(root.right, path + '->' + str(root.right.val))
+                
+        # Check if tree is empty
+        if not root:
+            return result
+        
+        # Run DFS on tree
+        dfs(root, str(root.val))
+        
+        return result
+            
