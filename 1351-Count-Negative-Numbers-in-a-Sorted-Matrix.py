b383687e9ceaa3a51be92d7499b595729fcee208
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-06 12:19:49 -0600

    Add description

--- 1351-Count-Negative-Numbers-in-a-Sorted-Matrix.py
+++ 1351-Count-Negative-Numbers-in-a-Sorted-Matrix.py
@@ -1,3 +1,10 @@
+'''
+Leetcode Q1351- https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+
+Given a m * n matrix grid which is sorted in non-increasing order both row-wise and column-wise.
+Return the number of negative numbers in grid.
+'''
+
 class Solution:
     def findFirstNegIndex(self, arr):
         start = 0
