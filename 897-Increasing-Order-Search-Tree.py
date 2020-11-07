c256f5fd80efb5d8de136c83c69483e8ce127811
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-07 10:31:36 -0600

    AddQ897

--- 897-Increasing-Order-Search-Tree.py
+++ 897-Increasing-Order-Search-Tree.py
@@ -4,3 +4,17 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        # Check if tree is empty
+        if not root:
+            return root
+        
+        def traverseNodes(root: TreeNode):
+            # Check if no more nodes to traverse
+            if not root:
+                return []
+            # Traverse nodes In-order
+            return traverseNodes(root.left) + [root.val] + traverseNodes(root.right)
+            
