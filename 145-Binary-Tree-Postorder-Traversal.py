39e3ec1c674524f4b1c7f6d70702d3e3d595916e
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-07 14:30:48 -0600

    Add solution

--- 145-Binary-Tree-Postorder-Traversal.py
+++ 145-Binary-Tree-Postorder-Traversal.py
@@ -4,3 +4,10 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        
+        return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
