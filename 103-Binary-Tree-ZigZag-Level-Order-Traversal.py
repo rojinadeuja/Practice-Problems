f12dc1172e273e6ba4c25825e0ed7b1bdee589cb
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-07 17:42:54 -0600

    Add comments

--- 103-Binary-Tree-ZigZag-Level-Order-Traversal.py
+++ 103-Binary-Tree-ZigZag-Level-Order-Traversal.py
@@ -8,33 +8,43 @@
 from queue import Queue
 class Solution:
     def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
-        
-        resultList = []
         innerList = []
+        resultList = []
+
+        # If tree is empty return empty list
         if not root:
-            return resultList
+            return []
         
+        # Count number of nodes in each level, initialize 1 for root
         count = 1
         switch = False
+
+        # Intialize queue
         que = Queue()
+        # Put root into queue
         que.put(root)
         
         while not que.empty():
+            # Pop a node and decrement count
             root = que.get()
             count -= 1
-           
+            # Add its children to queue
             if root.left:
                 que.put(root.left)
             if root.right:
                 que.put(root.right)
-           
+            # Add node to innerList
             innerList.append(root.val)
-            
+
+            # If no more nodes in level, add level to resultList
             if count == 0:
+                # If switch, reverse the innerList
                 if switch:
                     innerList.reverse()
                 resultList.append(innerList)
                 innerList = []
                 count = que.qsize()
+                # Toggle switch after each level
                 switch = not switch
+
         return resultList
