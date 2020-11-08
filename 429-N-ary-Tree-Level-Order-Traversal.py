a2ef6d69fd16914960a0d9012d1b29eb99fcbf48
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-07 23:04:46 -0600

    Add solution

--- 429-N-ary-Tree-Level-Order-Traversal.py
+++ 429-N-ary-Tree-Level-Order-Traversal.py
@@ -5,3 +5,36 @@ class Node:
         self.val = val
         self.children = children
 """
+
+from queue import Queue
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        resultList = []
+        innerList = []
+        
+        if not root:
+            return resultList
+        
+        que = Queue()
+        que.put(root)
+        
+        count = 1
+        
+        while not que.empty():
+            node = que.get()
+            count -= 1
+            
+            if node.children:
+                children = node.children
+                for child in children:
+                    que.put(child)
+                
+            innerList.append(node.val)
+            
+            if count == 0:
+                resultList.append(innerList)
+                innerList = []
+                count = que.qsize()
+                
+        return resultList
