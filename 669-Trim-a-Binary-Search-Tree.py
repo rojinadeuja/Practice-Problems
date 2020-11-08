e9913692bd207f070b269ab4ce4332dec49ed5e7
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-08 11:45:27 -0600

    Add solution

--- 669-Trim-a-Binary-Search-Tree.py
+++ 669-Trim-a-Binary-Search-Tree.py
@@ -5,4 +5,18 @@
 #     self.right = right
 
 class Solution:
-    def TrimBST(root: TreeNode, low: int, high: int) -> TreeNode:
+    def trimBST(self, root: TreeNode, low: int, high: int) -> TreeNode:
+        
+        if not root:
+            return None
+        
+        if root.val < low:
+            return self.trimBST(root.right, low, high)
+            
+        elif root.val > high:
+            return self.trimBST(root.left, low, high)
+            
+        else:
+            root.left = self.trimBST(root.left, low, high)
+            root.right = self.trimBST(root.right, low, high)
+            return root
