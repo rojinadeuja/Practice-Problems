a2ac5911c58873ae053d42729edd4c85c0d90af4
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-08 10:14:51 -0600

    Add comments

--- 704-Binary-Search.py
+++ 704-Binary-Search.py
@@ -7,14 +7,17 @@ If target exists, then return its index, otherwise return -1.
 '''
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        # Use Binary Search on array
         start = 0
         end = len(nums)-1
         while start<=end:
             median = (start + end ) // 2
+            # If found, return index
             if target == nums[median]:
                 return median
             elif target > nums[median]:
                 start = median + 1
             else:
                 end = median - 1
+        # If not found, return -1
         return -1
