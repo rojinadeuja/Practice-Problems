401bf1d0bb76924c96da648fd0acc07168278e46
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-08 14:11:51 -0600

    Add solution

--- 501-Find-Mode-in-Binary-Search-Tree.py
+++ 501-Find-Mode-in-Binary-Search-Tree.py
@@ -4,3 +4,42 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
+class Solution:
+    def findMode(self, root: TreeNode) -> List[int]:
+
+        self.maxCount = 0
+        self.dct = {}
+        self.mode = []
+        
+        def findCount(root):
+           
+            if not root:
+                return
+            
+            if root.val not in self.dct:
+                self.dct[root.val] = 1
+            else:
+                self.dct[root.val] += 1
+            
+            if root.left:
+                findCount(root.left)
+                
+            if root.right:
+                findCount(root.right)
+                
+            if self.dct[root.val] >= self.maxCount:
+                self.maxCount = self.dct[root.val]
+                
+            if not root.right and not root.left:
+                self.count = 1
+            
+        
+        findCount(root)
+        
+        for key, val in self.dct.items():
+            if val == self.maxCount:
+                self.mode.append(key)
+                
+        return self.mode
