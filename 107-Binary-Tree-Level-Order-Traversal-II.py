88cd7a8d3be56f27d339722dcf2895ade9051ca9
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-07 23:47:21 -0600

    Add solution

--- 107-Binary-Tree-Level-Order-Traversal-II.py
+++ 107-Binary-Tree-Level-Order-Traversal-II.py
@@ -3,3 +3,33 @@
 #     self.val = val
 #     self.left = left
 #     self.right = right
+
+from queue import Queue
+    
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        resultList = []
+        innerList = []
+        
+        if not root:
+            return resultList
+        count = 1
+        que = Queue()
+        que.put(root)
+        
+        while not que.empty():
+            node = que.get()
+            count -= 1
+            
+            if node.left:
+                que.put(node.left)
+            if node.right:
+                que.put(node.right)
+            
+            innerList.append(node.val)
+            
+            if count == 0:
+                resultList.insert(0, innerList)
+                innerList = []
+                count = que.qsize()
+        return resultList
