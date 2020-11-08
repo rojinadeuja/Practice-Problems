a677270e74db9540b6b524f2b8c76c2bdb2a71bf
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-08 10:13:40 -0600

    Add description

--- 704-Binary-Search.py
+++ 704-Binary-Search.py
@@ -1,3 +1,10 @@
+'''
+Leetcode Q704- https://leetcode.com/problems/binary-search/
+
+Given a sorted (in ascending order) integer array nums of n elements and a target value, 
+write a function to search target in nums. 
+If target exists, then return its index, otherwise return -1.
+'''
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         start = 0
