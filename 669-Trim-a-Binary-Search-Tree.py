3467162347fac93fb6b2a013f9d3ebcfaeb750ed
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-08 11:45:45 -0600

    Add comments

--- 669-Trim-a-Binary-Search-Tree.py
+++ 669-Trim-a-Binary-Search-Tree.py
@@ -10,13 +10,18 @@ class Solution:
         if not root:
             return None
         
+        # print("Root is",root.val)
+          
         if root.val < low:
+            # Process only its right
             return self.trimBST(root.right, low, high)
             
         elif root.val > high:
+            # Process only its left
             return self.trimBST(root.left, low, high)
             
         else:
+            # Root is okay in it's place
             root.left = self.trimBST(root.left, low, high)
             root.right = self.trimBST(root.right, low, high)
             return root
