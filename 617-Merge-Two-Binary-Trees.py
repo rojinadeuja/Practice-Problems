5f169a5e63b0b20b0ab83e2c40e08fb3bc15d14a
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-07 23:04:24 -0600

    Add Q429

--- 617-Merge-Two-Binary-Trees.py
+++ 617-Merge-Two-Binary-Trees.py
@@ -30,3 +30,5 @@ class Solution:
         t1.left = self.mergeTrees(t1.left, t2.left)
         t1.right = self.mergeTrees(t1.right, t2.right)
         return t1
+
+# Time complexity is O(n). Space complexity is O(n)
