9e9952a415b18f0485ca76e4a5ee693c6b258772
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-08 11:44:15 -0600

    Add solution

---
+++ 669-Trim-a-Binary-Search-Tree.py
@@ -0,0 +1,8 @@
+# Definition for a binary tree node
+# class TreeNode (self, val=0, left=None, right=None):
+#     self.val = val 
+#     self.left = left
+#     self.right = right
+
+class Solution:
+    def TrimBST(root: TreeNode, low: int, high: int) -> TreeNode:
