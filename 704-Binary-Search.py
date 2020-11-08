3052a172d552ef0325ca0193be7484726a716293
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-08 10:12:49 -0600

    Add solution

--- 704-Binary-Search.py
+++ 704-Binary-Search.py
@@ -1,3 +1,13 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
+        start = 0
+        end = len(nums)-1
+        while start<=end:
+            median = (start + end ) // 2
+            if target == nums[median]:
+                return median
+            elif target > nums[median]:
+                start = median + 1
+            else:
+                end = median - 1
+        return -1
