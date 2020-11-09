b8e2e0df67e2551271078206d8c5569b8c1374e7
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-09 14:32:54 -0600

    Add description

--- 344-Reverse-String.py
+++ 344-Reverse-String.py
@@ -1,3 +1,11 @@
+'''
+Leetcode Q344- https://leetcode.com/problems/reverse-string/
+
+Write a function that reverses a string. The input string is given as an array of characters char[].
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+You may assume all the characters consist of printable ascii characters.
+
+'''
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         start = 0
