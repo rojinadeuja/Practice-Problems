8938264ff689e7f5ad6b0a63632c96f54f3e4270
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-09 13:34:44 -0600

    Add Approach 1

--- 123-Valid-Panlindrome.py
+++ 123-Valid-Panlindrome.py
@@ -5,3 +5,20 @@ Given a string, determine if it is a palindrome, considering only alphanumeric c
 
 Note: For the purpose of this problem, we define empty string as valid palindrome.
 '''
+
+# Approach 1
+import re
+class Solution:
+    def isPalindrome(self, s:str) -> bool:
+        if not str:
+            return True
+        s = re.sub(r'[\W_]+','',s).lower()
+        start = 0
+        end = len(s)-1
+        while start <= end:
+            if s[start] != s[end]:
+                return False
+            else:
+                start +=1
+                end -=1
+        return True
