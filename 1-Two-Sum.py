8856a898930870e30867aca79538d6cc12ac27c0
rojinadeuja <rojinadeuja33g@gmail.com>
2021-08-21 19:06:21 -0500

    Add solution using Dict

--- 1-Two-Sum.py
+++ 1-Two-Sum.py
@@ -5,28 +5,28 @@ Given an array of integers nums and an integer target, return indices of the two
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 You can return the answer in any order.
-'''
-
-# # Brute-Force
-# class Solution(object):
-#     def twoSum(self, nums, target):
-#         """
-#         :type nums: List[int]
-#         :type target: int
-#         :rtype: List[int]
-#         """
-#         result = []
-#         for i in range(0, len(nums)):
-#             for j in range(1, len(nums)-1):
-#                 sum = nums[i] + nums[j]
-#                 if sum == target:
-#                     result.append(i)
-#                     result.append(j)
-#         return result
-#     # Run time complexity O(n*n), Space complexity O(1)
-                
+''' 
+# Further Optimized - Using a dictionary
+class Solution(object):
+    def twoSum(self, nums, target):
         
-# Optimized
+        numsDict = {}
+        for i, num in enumerate(nums):
+            if num not in numsDict:
+                numsDict[num] = [i]
+            else:
+                numsDict[num].append(i)
+
+        for num in numsDict.keys():
+            diff = target-num
+            if diff in numsDict:
+                if diff != num:
+                    return [numsDict[num][0], numsDict[diff][0]]
+                else:
+                    return [numsDict[num][0], numsDict[num][1]]
+# RC: O(n); SC: O(n)
+
+# Optimized - Using two pointers by sorting the array
 class Solution(object):
     def twoSum(self, nums, target):
         numsPrev = []
@@ -47,3 +47,22 @@ class Solution(object):
                 p1+=1
         return []
 # RC: O(n log n); SC: O(n)
+
+# Brute-Force
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        result = []
+        for i in range(0, len(nums)):
+            for j in range(1, len(nums)-1):
+                sum = nums[i] + nums[j]
+                if sum == target:
+                    result.append(i)
+                    result.append(j)
+        return result
+    # Run time complexity O(n*n), Space complexity O(1)
+                
